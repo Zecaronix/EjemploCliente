1391ef87d43cbbff1b5b05e91984f265ed18c113
Zecaronix <jcpazf@gmail.com>
2025-04-27 01:14:22 -0500

    sventas - clase 1 / 4

--- src/main/java/com/sventas/sventas/SventasApplication.java
+++ src/main/java/com/sventas/sventas/SventasApplication.java
@@ -3,11 +3,21 @@ package com.sventas.sventas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class of the Spring Boot application.
+ * Marks this class as a Spring Boot application using the @SpringBootApplication annotation,
+ * which combines @Configuration, @EnableAutoConfiguration, and @ComponentScan.
+ */
+@SuppressWarnings("checkstyle:Indentation")
 @SpringBootApplication
 public class SventasApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SventasApplication.class, args);
-	}
+  /**
+   * Main method that starts the Spring Boot application.
+   *
+   * @param args Command-line arguments passed when starting the application.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(SventasApplication.class, args);
+  }
 
 }
