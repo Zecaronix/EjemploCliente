8e4a119898fe6f4f163ce53b84a1292d2498cacf
Zecaronix <jcpazf@gmail.com>
2025-04-27 01:14:35 -0500

    sventas - clase 1 / 4

--- src/main/java/com/sventas/sventas/model/Cliente.java
+++ src/main/java/com/sventas/sventas/model/Cliente.java
@@ -1,75 +1,58 @@
 package com.sventas.sventas.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+/**
+ * Entity that represents a product in the database.
+ */
 @Entity
-@Table(name="cliente")
+@Table(name = "producto")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idCliente;
+  /**
+   * Unique identifier for the product.
+   * This field is automatically generated using the IDENTITY strategy.
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer idProducto;
 
-    @Column(name = "nombres",nullable = false,length = 70)
-    private String nombres;
+  /**
+   * Name of the product.
+   * This field is mandatory and has a maximum length of 100 characters.
+   */
+  @Column(name = "nombre", nullable = false, length = 100)
+  private String nombre;
 
-    @Column(name = "apellidos",nullable = false,length = 150)
-    private String apellidos;
+  /**
+   * Description of the product.
+   * This field is optional and has a maximum length of 255 characters.
+   */
+  @Column(name = "descripcion", nullable = true, length = 255)
+  private String descripcion;
 
-    @Column(name = "direccion",nullable = true,length = 150)
-    private String direccion;
+  /**
+   * Price of the product.
+   * This field is mandatory.
+   */
+  @Column(name = "precio", nullable = false)
+  private Double precio;
 
-    @Column(name = "telefono",nullable = true,length = 9)
-    private String telefono;
-
-    @Column(name = "email",nullable = true,length = 55)
-    private String email;
-
-    public Integer getIdCliente() {
-        return idCliente;
-    }
-
-    public void setIdCliente(Integer idCliente) {
-        this.idCliente = idCliente;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  /**
+   * Stock quantity of the product.
+   * This field is mandatory.
+   */
+  @Column(name = "stock", nullable = false)
+  private Integer stock;
 }
